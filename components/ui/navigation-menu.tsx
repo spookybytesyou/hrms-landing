832d2fd993a5c8bd7bd118ffd03f5be4e7df5f6b
spooky <spookyeats4skin@gmail.com>
2026-06-17 00:04:23 +0530

    make navbar hover off animation snappy and smoother

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -108,7 +108,7 @@ function NavigationMenuViewport({
       <NavigationMenuPrimitive.Viewport
         data-slot="navigation-menu-viewport"
         className={cn(
-          "origin-top-center relative mt-1.5 h-(--radix-navigation-menu-viewport-height) w-full overflow-hidden rounded-lg bg-popover text-popover-foreground shadow ring-1 ring-foreground/10 duration-100 md:w-(--radix-navigation-menu-viewport-width) data-open:animate-in data-open:zoom-in-90 data-closed:animate-out data-closed:zoom-out-90",
+          "origin-top-center relative mt-1.5 h-(--radix-navigation-menu-viewport-height) w-full overflow-hidden rounded-lg bg-popover text-popover-foreground shadow ring-1 ring-foreground/10 duration-100 md:w-(--radix-navigation-menu-viewport-width) data-open:animate-in data-open:duration-150 data-open:zoom-in-95 data-closed:animate-out data-closed:duration-75 data-closed:zoom-out-95 data-closed:fade-out-0",
           className
         )}
         {...props}
